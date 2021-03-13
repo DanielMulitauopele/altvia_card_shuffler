f863a0a561da63ee98a56c9fb12c222edc20b32e
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-12 21:36:49 -0700

    Adds deck class and asserts existence

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,6 +4,10 @@ require './lib/deck'
 
 class DeckTest < MiniTest::Test 
     def setup
+        @deck = Deck.new
+    end
 
+    def test_it_exists
+        assert_instance_of Deck, @deck
     end
 end
