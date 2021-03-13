655172b103a26a6d942e4d5f82293ea1b56f79fa
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-13 11:59:21 -0700

    Adds welcome message functionality and tests

--- lib/card_shuffler.rb
+++ lib/card_shuffler.rb
@@ -1,3 +1,6 @@
 class CardShuffler
-
+    def welcome_message
+        puts "Welcome to the Altvia Card Shuffler!"
+        puts "other stuff"
+    end
 end
--- test/card_shuffler_test.rb
+++ test/card_shuffler_test.rb
@@ -3,10 +3,14 @@ require './lib/card_shuffler'
 
 class CardShufflerTest < MiniTest::Test 
     def setup
-        @cs = CardShuffler.new
+        @shuffler = CardShuffler.new
     end
 
     def test_it_exists
-        assert_instance_of CardShuffler, @cs 
+        assert_instance_of CardShuffler, @shuffler 
+    end
+
+    def test_it_displays_welcome_message
+        assert_output(/Welcome to the Altvia Card Shuffler!\n/) { @shuffler.welcome_message }
     end
 end
