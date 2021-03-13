5233b6aa97002358c54bd4cb482ac5f5f73f38cc
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-13 12:30:52 -0700

    Adds input command functionality and tests

--- lib/card_shuffler.rb
+++ lib/card_shuffler.rb
@@ -1,6 +1,47 @@
+require './lib/card'
+require './lib/deck'
+
 class CardShuffler
+    attr_reader :deck
+
+    def initialize
+        initial_card = Card.new("Spades", "Ace")
+        @deck = Deck.new(initial_card)
+    end
+
     def welcome_message
         puts "Welcome to the Altvia Card Shuffler!"
-        puts "other stuff"
+        puts "This program shuffles cards..."
+        puts "but that's a little boring. Wanna see a magic trick instead?"
+        puts "Type Y if you, or N if you're a lame and I'll just leave you alone."
+    end
+
+    def start 
+        while true
+            user_input = gets.chomp
+            process_input(user_input)
+        end
+    end
+
+    def process_input(user_input)
+        input_commands[user_input.to_sym].call
+    end
+
+    private
+
+    def input_commands
+        {
+            'Y': method(:run_magic_trick),
+            'N': method(:quit)
+        }
+    end
+
+    def run_magic_trick
+        puts "Ah sweet! You're a gamer."
+        puts "Ok let's do it."
+    end
+
+    def quit
+        abort "Ah fine. It's not like my feelings are hurt or anything. Bye, I guess."
     end
 end
--- test/card_shuffler_test.rb
+++ test/card_shuffler_test.rb
@@ -10,7 +10,15 @@ class CardShufflerTest < MiniTest::Test
         assert_instance_of CardShuffler, @shuffler 
     end
 
+    def test_it_starts_with_a_deck
+        assert_instance_of Deck, @shuffler.deck
+    end
+
     def test_it_displays_welcome_message
         assert_output(/Welcome to the Altvia Card Shuffler!\n/) { @shuffler.welcome_message }
+    end
+
+    def test_it_can_process_a_yes
+        assert_output(/Ah sweet! You're a gamer.\n/) { @shuffler.process_input('Y') }
     end
 end
