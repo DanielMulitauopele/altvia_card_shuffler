47a3a9b5544ada5d94d674db5a5f21473df0ac47
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-12 21:52:30 -0700

    Adds add functionality and tests: added for readability even though methods already exist for arrays

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,11 @@
 class Deck
     attr_accessor :cards
-    
+
     def initialize(initial_card)
         @cards = [initial_card]
+    end
+
+    def add(new_card)
+        @cards << new_card
     end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,6 +5,9 @@ require './lib/deck'
 class DeckTest < MiniTest::Test 
     def setup
         @card1 = Card.new("Hearts", "Nine")
+        @card2 = Card.new("Diamonds", "Queen")
+        @card3 = Card.new("Spades", "Ace")
+
         @deck = Deck.new(@card1)
     end
 
@@ -15,6 +18,16 @@ class DeckTest < MiniTest::Test
     def test_it_starts_with_at_least_one_card
         expected = "Nine of Hearts"
         actual = @deck.cards.first.name
+
+        assert_equal expected, actual 
+    end
+
+    def test_it_can_add_more_cards
+        @deck.add(@card2)
+        @deck.add(@card3)
+
+        expected = 3
+        actual = @deck.cards.count
 
         assert_equal expected, actual 
     end
