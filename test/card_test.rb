e70882937ed171976d354c65fc5a6495a05c9cc8
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-12 18:15:40 -0700

    Adds more tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -9,4 +9,20 @@ class CardTest < MiniTest::Test
     def test_it_exists
         assert_instance_of Card, @card
     end
+
+    def test_it_has_a_suit
+        
+    end
+
+    def test_it_has_a_value
+        skip
+    end
+
+    def test_it_has_a_color
+        skip
+    end
+
+    def test_it_has_a_type
+        skip
+    end
 end
